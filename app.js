3d4b12f7f0266cf318b1079ae856a61a55787d05
Kurt <ksphotopro@gmail.com>
2017-03-09 01:45:46 -0500

    working version - needs cleanup

--- app.js
+++ app.js
@@ -40,7 +40,16 @@ function bindOnClick(bindTo){
 }
 
 let clickTile = function(){
+	if (this.style.background === "rgb(255, 255, 255)"){
+		return
+	}
+	gameState.boardReset();
+	this.style.color = "rgba(0,0,0,1)";
 	let move = this.innerHTML;
+	gameState.matchTrack.push(this);
+	if (gameState.matchTrack.length > 2){
+		gameState.matchTrack.shift();
+	}
 	gameState.tileCompare.push(move);
 	if (gameState.tileCompare.length > 2){
 		gameState.tileCompare.shift();
@@ -53,21 +62,39 @@ const gameState = {
 	matchesLeft: 6,
 	tries: 0,
 	tileCompare: [],
+	matchTrack: [],
 	guessPlus: function(){
 		this.tries++;
 		document.getElementById("tryCount").innerHTML = this.tries;
 	},
+	
 	scoreCheck: function(){
 		if (this.tileCompare[1] === this.tileCompare[0]){
 			this.matchesLeft--;
 			document.getElementById("leftCountdown").innerHTML = this.matchesLeft;
+			this.matchTrack[0].style.background = "rgba(255,255,255,1)";
+			this.matchTrack[0].style.color = "rgba(0,0,0,1)";
+			this.matchTrack[0].children[0].className = "matched"
+			this.matchTrack[1].style.background = "rgba(255,255,255,1)";
+			this.matchTrack[1].style.color = "rgba(0,0,0,1)";
+			this.matchTrack[1].children[0].className = "matched"
+
 		}
-		console.log(this.matchesLeft);
-	}
-	
+		return
+	},
+	boardReset: function(){
+		let board = document.getElementById("game-grid").children;
+		for (let i = 0; i < board.length; i++){
+			if (board[i].children[0].className === "matched"){
+				continue
+			} 
+			console.log(board[i].style.background);
+			board[i].style.color = "rgba(0,0,0,0)";
+			
+		}
+	}	
 }
-//3.0 - track player score
-//4.0 - track game state
+
 
 //run
 //console.log(initialTiles);
